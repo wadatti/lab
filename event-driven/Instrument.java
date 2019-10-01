74d5ec86022b1c5bc9916cf32f1a99259524d2d0
wadatti <22770733+wadatti@users.noreply.github.com>
2019-10-01 14:36:23 +0900

    add automatic Runnable wrapped

--- event-driven/Instrument.java
+++ event-driven/Instrument.java
@@ -2,23 +2,11 @@ import javassist.*;
 import javassist.expr.ExprEditor;
 import javassist.expr.MethodCall;
 
+import java.io.File;
+import java.io.FilenameFilter;
 import java.io.IOException;
 
 public class Instrument {
-    private static void InstrumentTask() throws NotFoundException, CannotCompileException, IOException {
-        ClassPool classPool = ClassPool.getDefault();
-        CtClass taskClass = classPool.get("Task");
-        CtClass wrapperClass = classPool.get("WrapperRunnable");
-        taskClass.setSuperclass(wrapperClass);
-        CtMethod runMethod = taskClass.getDeclaredMethod("run");
-        runMethod.insertBefore(" MyLogger.writeLog(\"BeginEvent(\" + this.eventKind +\",\" + Thread.currentThread().getId() +\",\" + this.eventID +\")\");");
-        taskClass.writeFile();
-    }
-
-
-//    java.io.FileWriter fw = new java.io.FileWriter("log.txt", true);
-//    fw.write("BeginEvent(" + this.eventKind +"," + java.lang.Thread().getID() +"," + this.eventID +")\n");
-//    fw.close();
 
     private static void InstrumentMyThreadPool() throws CannotCompileException, NotFoundException, IOException {
         ClassPool classPool = ClassPool.getDefault();
@@ -33,11 +21,6 @@ public class Instrument {
 
     }
 
-
-//    MyThreadPool.execute insertbefore
-//    java.io.FileWriter fw = new java.io.FileWriter("log.txt", true);
-//    fw.write("createEvent(" + ((WrapperRunnable)$1).eventKind + "," + Thread.currentThread().getId() + "," + ((WrapperRunnable)$1).eventID + ")\n");
-//    fw.close();
 
     private static void InstrumentMain() throws NotFoundException, CannotCompileException, IOException {
         ClassPool classPool = ClassPool.getDefault();
@@ -67,7 +50,35 @@ public class Instrument {
         });
         mainClass.writeFile();
     }
-    
+
+    private static void runnableInstrument() throws NotFoundException, CannotCompileException, IOException {
+        ClassPool classPool = ClassPool.getDefault();
+        File file = new File("./src");
+        File[] files = file.listFiles(new FilenameFilter() {
+            @Override
+            public boolean accept(File dir, String name) {
+                boolean flag = true;
+                if (name.equals(".DS_Store") || name.equals("WrapperRunnable.java") || name.equals("Instrument.java") || name.equals("DispatchTask.java"))
+                    flag = false;
+                return flag;
+            }
+        });
+        for (File classFile : files) {
+            CtClass cc = classPool.get(classFile.getName().substring(0, classFile.getName().lastIndexOf(".")));
+            CtClass[] interfaces = cc.getInterfaces();
+            for (CtClass tmp : interfaces) {
+                if (tmp.getName().equals("java.lang.Runnable")) {
+                    CtClass wrapperClass = classPool.get("WrapperRunnable");
+                    cc.setSuperclass(wrapperClass);
+                    System.out.println("wralled:" + cc.getName());
+                    CtMethod runMethod = cc.getDeclaredMethod("run");
+                    runMethod.insertBefore(" MyLogger.writeLog(\"BeginEvent(\" + this.eventKind +\",\" + Thread.currentThread().getId() +\",\" + this.eventID +\")\");");
+                    cc.writeFile();
+                }
+            }
+        }
+    }
+
 
 //    MyThreadPool.execute replace
 //    {
@@ -79,7 +90,7 @@ public class Instrument {
 //    }
 
     public static void main(String[] args) throws NotFoundException, CannotCompileException, IOException {
-        InstrumentTask();
+        runnableInstrument();
         InstrumentMyThreadPool();
         InstrumentMain();
         System.out.println("instrument!!");
