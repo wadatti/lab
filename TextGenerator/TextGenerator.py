4563d81b569cfd6122bb500b805f7857618b47cd
wadatti <22770733+wadatti@users.noreply.github.com>
2020-03-12 19:41:47 +0900

    update TextGenerator

--- TextGenerator/TextGenerator.py
+++ TextGenerator/TextGenerator.py
@@ -1,6 +1,19 @@
 import string
 
-path = './word.txt'
 
-for s in string.ascii_letters:
-    print(s)
+def permutation(q, ans, f):
+    if (len(q) <= 1):
+        f.write(ans + q + " ")
+    else:
+        for i in range(len(q)):
+            permutation(q[0:i]+q[i+1:], ans+q[i], f)
+
+
+path = './words.txt'
+
+f = open(path, mode='w')
+
+for i in range(8):
+    permutation(string.ascii_letters[0:i], "", f)
+    f.write('\n')
+f.close()
